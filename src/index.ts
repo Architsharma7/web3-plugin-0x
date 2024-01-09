ce337dc3a3dd1611c7820a68ba09b8aa61d1594a
Architsharma7 <architsharma7911@gmail.com>
2024-01-09 23:15:58 +0530

    added finalised 0x plugin

--- src/index.ts
+++ src/index.ts
@@ -1,184 +1,144 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Web3, Web3PluginBase } from "web3";
-import { ChainIDs, SwapParams } from "./types";
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Web3PluginBase, Web3 } from "web3";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ERC20TokenContract } from "@0x/contract-wrappers";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BigNumber } from "@0x/utils";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import qs from "qs";
+import { Chains, SwapParams } from "./types";
 
-export class OneInchPlugin extends Web3PluginBase {
-  public pluginNamespace = "OneInch";
-
+export class ZeroXSwapPlugin extends Web3PluginBase {
+  public pluginNamespace = "zeroXSwap";
+  private apiKey: string;
+  private defaultParams: SwapParams;
   private web3: any;
+  private takerPrivateKey: string;
+  private chain: Chains;
 
-  public constructor(
-    private apiKey: string,
-    private chainId: ChainIDs,
-    private web3RpcUrl: string,
-    private walletAddress: string
+  constructor(
+    apiKey: string,
+    defaultParams: SwapParams,
+    takerPrivateKey: string,
+    chain: Chains
   ) {
     super();
-    // // @ts-ignore
-    // this.apiKey = options?.apiKey;
-    // // @ts-ignore
-    // this.chainId = options?.chainId;
-    // // @ts-ignore
-    // this.web3RpcUrl = options?.web3RpcUrl;
-    // // @ts-ignore
-    // this.walletAddress = options?.walletAddress;
-    this.web3 = new Web3(this.web3RpcUrl);
+    this.apiKey = apiKey;
+    this.defaultParams = defaultParams;
+    this.web3 = new Web3();
+    this.takerPrivateKey = takerPrivateKey;
+    this.chain = chain;
   }
 
-  private async apiRequest(
-    url: string,
-    method: string = "GET",
-    body?: any
-  ): Promise<any> {
-    const headers = {
-      Authorization: `Bearer ${this.apiKey}`,
-      "Content-Type": "application/json",
-    };
-
-    const requestOptions: any = {
-      method,
-      headers,
-    };
-
-    if (body) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      requestOptions.body = JSON.stringify(body);
+  private async signAndSendTransaction(txObject: any): Promise<string> {
+    if (!this.takerPrivateKey) {
+      throw new Error("Taker's private key not set");
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    const response = await fetch(url, requestOptions);
-    console.log("Response: ", response);
-
-    if (!response.ok) {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      console.log(`API request failed with status ${response}`);
-    }
-
-    // Check if the response body is empty
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const text = await response.text();
-    console.log("Response: ", text);
-    if (!text) {
-      console.log(response);
-      throw new Error("Empty response from the server");
-    }
-
-    try {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument
-      const responseBody = JSON.parse(text);
-      console.log(responseBody);
-      return responseBody;
-    } catch (error: any) {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-member-access
-      throw new Error(`Failed to parse JSON response: ${error?.message}`);
-    }
-  }
-
-  private async signAndSendTransaction(
-    transaction: any,
-    // from: string,
-    privateKey: string
-  ): Promise<string> {
-    console.log(transaction);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const { rawTransaction } = await this.web3.eth.accounts.signTransaction(
-      transaction,
-      privateKey,
+    const signedTransaction = await this.web3.eth.accounts.signTransaction(
+      txObject,
+      this.takerPrivateKey
     );
 
-    return await this.broadCastRawTransaction(rawTransaction);
-  }
-
-  private async broadCastRawTransaction(rawTransaction: any): Promise<any> {
-    const broadcastApiUrl = `https://api.1inch.dev/tx-gateway/v1.1/${this.chainId}/broadcast`;
-    return (
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      this.apiRequest(broadcastApiUrl, "POST", { rawTransaction })
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        .then((res: any) => res.transactionHash)
+    const receipt = await this.web3.eth.sendSignedTransaction(
+      signedTransaction.rawTransaction
     );
+
+    return receipt.transactionHash;
   }
 
-  public async checkAllowance(
-    tokenAddress: string,
-    walletAddress: string
-  ): Promise<boolean> {
-    const url = `https://api.1inch.dev/swap/v5.2/${this.chainId}/approve/allowance?tokenAddress=${tokenAddress}&walletAddress=${walletAddress}`;
+  public async tokenAllowance(
+    overrideParams: Partial<{
+      sellToken: string;
+      buyToken: string;
+      sellAmount: string;
+      takerAddress: string;
+    }> = {}
+  ): Promise<void> {
+    const params = { ...this.defaultParams, ...overrideParams };
+    const res = await fetch(
+      `https://${this.chain}api.0x.org/swap/v1/quote?${qs.stringify(params)}`
+    );
+    const quote = await res.json();
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const response = await this.apiRequest(url);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    return response.allowance;
-  }
+    const tokenContract = new ERC20TokenContract(
+      params.sellToken,
+      this.web3.currentProvider
+    );
+    const maxApproval = new BigNumber(2).pow(256).minus(1);
 
-  public async approveToken(
-    tokenAddress: string,
-    amount: string,
-    privateKey: string
-  ): Promise<string> {
-    const url = `https://api.1inch.dev/swap/v5.2/${this.chainId}/approve/transaction?tokenAddress=${tokenAddress}&amount=${amount}`;
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const headers = {
-      headers: {
-        Authorization: `Bearer ${this.apiKey}`,
-        accept: "application/json",
-      },
-    };
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const transaction = await fetch(url, headers);
-    console.log("Token Approval Transaction: ", transaction);
+    const approvalTxData = tokenContract
+      .approve(quote.allowanceTarget, maxApproval)
+      .getABIEncodedTransactionData();
 
-    if (transaction == null || undefined) {
-      console.log("Transaction is null or undefined");
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const gasLimit = await this.web3.eth.estimateGas({
-      ...transaction,
-      from: this.walletAddress,
+    const txdata = await this.signAndSendTransaction({
+      from: params.takerAddress,
+      to: params.sellToken,
+      data: approvalTxData,
     });
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const approvedTransaction = {
-      ...transaction,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      gas: gasLimit,
-    };
-
-    const approveTxHash = await this.signAndSendTransaction(
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      approvedTransaction,
-      privateKey
-    );
-    console.log("Approve tx hash: ", approveTxHash);
-    return approveTxHash;
+    console.log(txdata);
   }
 
-  public async swapTokens(
-    swapParams: SwapParams,
-    privateKey: string
-  ): Promise<string> {
-    const url = `https://api.1inch.dev/swap/v5.2/${this.chainId}/swap`;
+  public async getPrice(
+    overrideParams: Partial<{
+      sellToken: string;
+      buyToken: string;
+      sellAmount: string;
+      takerAddress: string;
+    }> = {}
+  ): Promise<any> {
+    const params = { ...this.defaultParams, ...overrideParams };
+    const headers = { "0x-api-key": this.apiKey };
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const swapTransaction = await this.apiRequest(url, "POST", swapParams)
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      .then((res: any) => res.tx);
-
-    const swapTxHash = await this.signAndSendTransaction(
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      swapTransaction,
-      privateKey
+    const response = await fetch(
+      `https://${this.chain}api.0x.org/swap/v1/price?${qs.stringify(params)}`,
+      { headers }
     );
-    return swapTxHash;
+
+    return await response.json();
+  }
+
+  public async swap(
+    overrideParams: Partial<{
+      sellToken: string;
+      buyToken: string;
+      sellAmount: string;
+      takerAddress: string;
+    }> = {}
+  ): Promise<any> {
+    const params = { ...this.defaultParams, ...overrideParams };
+    const headers = { "0x-api-key": this.apiKey };
+
+    const response = await fetch(
+      `https://${this.chain}api.0x.org/swap/v1/quote?${qs.stringify(params)}`,
+      { headers }
+    );
+
+    const quote = await response.json();
+
+    const receipt = await this.signAndSendTransaction({
+      from: quote.taker,
+      to: quote.to,
+      data: quote.data,
+      value: quote.value,
+      gasPrice: quote.gasPrice,
+    });
+
+    console.log(receipt);
+
+    return receipt;
   }
 }
 
 // Module Augmentation
 declare module "web3" {
   interface Web3Context {
-    OneInch: OneInchPlugin;
+    zeroXSwap: ZeroXSwapPlugin;
   }
 }
